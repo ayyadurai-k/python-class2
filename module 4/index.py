0623057e8cbf7a9e8cd0cb8df8d91f5f2da563b0
Ayyadurai <kmayyadurai286@gmailc.om>
2024-09-11 20:44:55 +0530

    Module 4 Finished

---
+++ module 4/index.py
@@ -0,0 +1,18 @@
+name = "Jeevitha"
+
+# INDEXING 
+new_name = name[7] # SQUARE BRACKET
+
+print(new_name)
+
+# SLICING
+
+new_name = name[1:6] #  INDEX TO  INDEX
+new_name = name[:6] # START TO INDEX
+new_name = name[3:] # INDEX TO END
+new_name = name[-1] # FROM LAST
+
+reversed_string = name[::-1] # REVERSE STRING
+
+
+print("reversed_string : ",reversed_string)
