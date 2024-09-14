4723d8ddf0149d7ca7565c2419a3c733a12edacd
Ayyadurai <kmayyadurai286@gmailc.om>
2024-09-14 20:12:47 +0530

    Tuple Finished

--- module 7/main.py
+++ module 7/main.py
@@ -1,7 +1,13 @@
 # TUPLES 
 
-numbers = (10,4,2,1)
+numbers = (10,4,2,2,2,1)
 
-numbers.sort()  # THIS WILL RAISE A ERROR  , BECAUSE TUPLE IN NOT CHANGEABLE (IMMUTABLE)
+# numbers.sort()  # THIS WILL RAISE A ERROR  , BECAUSE TUPLE IN NOT CHANGEABLE (IMMUTABLE)
 
-print(numbers)
+count = numbers.count(2)
+
+index = numbers.index(2)
+
+print("Count : ",count)
+
+print("Index : ",index)
