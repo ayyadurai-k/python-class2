4e1f28105f3d7e04ec766a8d74bdd1981f1660aa
Ayyadurai <kmayyadurai286@gmailc.om>
2024-09-16 20:41:12 +0530

    dictionary methods finished

--- module 7/dict_methods.py
+++ module 7/dict_methods.py
@@ -1,13 +1,45 @@
 person = {
     "name" : "Jeevitha",
     "role" : "Web Developer",
-    "skills" : "Django , Python"
+    "skills" : "Django , Python",
+    "age" : 23
 }
 
 
-print("Keys : ",list(person.keys())) # GET ALL KEYS IN DICTIONARY
-print("Values : ",list(person.values())) # GET ALL VALUES IN DICTIONARY
 
-# ITEMS 
 
-print("Items : ",list(person.items())) # LIST OF TUPLES WITH KEY AND VALUE
+# print("Keys : ",list(person.keys())) # GET ALL KEYS IN DICTIONARY
+# print("Values : ",list(person.values())) # GET ALL VALUES IN DICTIONARY
+
+# # ITEMS 
+
+# print("Items : ",list(person.items())) # LIST OF TUPLES WITH KEY AND VALUE
+
+# UPDATE 
+
+# data = {"age" : 23,"address" : "Chennai"}
+
+# data = {"city" : "Chennai"}
+
+# person["address"] = data["city"]
+
+# person.update(data)
+
+
+# print("Person : ",person)
+
+# POP  ITEM
+
+# removed_item = person.popitem()
+
+# print("removed_item : ",removed_item)
+
+# POP 
+
+# removed_item = person.pop("age",None)
+
+# print("removed_item : ",removed_item)
+
+print("Person : ",person)
+
+
---
+++ module 7/get.py
@@ -0,0 +1,9 @@
+person = {
+    "name" : "Jeevitha",
+    "role" : "Web Developer",
+    "skills" : "Django , Python",
+    "age" : 23
+}
+
+print("Get 1 : ",person["age"]) # GET 1
+print("Get 2 : ",person.get("age",18)) # GET 2
