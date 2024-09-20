cd8a1ca40928f7c99369725d48e0bccf910fc227
Ayyadurai <kmayyadurai286@gmailc.om>
2024-09-20 21:09:21 +0530

    module 9 finished

---
+++ module 9/fun.py
@@ -0,0 +1,17 @@
+
+
+# BREAK
+
+# for i in range(1,101):
+#     print(i,". Hello")
+
+#     if i == 50:
+#         break
+
+# CONTINUE
+
+for i in range(1,101):
+    if i == 50 :
+        continue
+
+    print(i,". Hello")
---
+++ module 9/main.py
@@ -0,0 +1,16 @@
+
+# NORMAL LOOP 
+for i in range(10):
+    print(i)
+
+# START AND END VALUE
+for i in range(5,21):
+    print(i)
+
+# START , END AND STEP VALUE
+for i in range(5,21,3):
+    print(i)
+
+# START , END AND STEP VALUE(NEGATIVE)
+for i in range(100,10,-10):
+    print(i)
