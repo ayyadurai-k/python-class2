7bf1399598bcfcf7e4069a6f23751fc7fd204931
Ayyadurai <kmayyadurai286@gmailc.om>
2024-09-24 20:24:31 +0530

    workout 1 finished

---
+++ module 11/workout.py
@@ -0,0 +1,13 @@
+
+
+def km_to_m(km):
+    answer = km*1000
+    return answer
+
+
+a = km_to_m(3) # 3000
+b = km_to_m(5) # 5000
+c = km_to_m(8) # 8000
+
+
+
---
+++ module 9/fibo.py
@@ -0,0 +1,12 @@
+a=0
+b=1
+
+print(a,b,end=" ")
+
+
+for i in range(8):
+    c = a+b 
+    print(c,end=" ")
+    a = b 
+    b = c 
+
